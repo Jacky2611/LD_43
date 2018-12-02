879af9c847ef1cef1afa7c27335716b31d5ecfcf
Jacky <jacky2611@gmail.com>
2018-12-02 13:10:30 +0100

    Added CameraManager

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+    Camera camera;
+    GameObject player;
+    Resolution res;
+
+
+    // Use this for initialization
+    void Start () {
+        camera = gameObject.GetComponent<Camera>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        res = Screen.currentResolution;
+        UpdateCameraScale();
+
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if(res.width != Screen.currentResolution.width)
+        {
+            res = Screen.currentResolution;
+            UpdateCameraScale();
+        }
+        camera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, camera.transform.position.z);
+	}
+
+    void UpdateCameraScale()
+    {
+        camera.orthographicSize= ((res.width/6) / 32 / 2);
+    }
+}
