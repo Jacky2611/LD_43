2a6c18e4775d26390c2a93ed449ae4d2e1d72810
Jacky <jacky2611@gmail.com>
2018-12-02 12:23:06 +0100

    Added Pathfinding

--- Assets/Scripts/MeleeEnemyController.cs
+++ Assets/Scripts/MeleeEnemyController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Pathfinding;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Animations;
 using UnityEngine;
@@ -12,6 +13,16 @@ public class MeleeEnemyController : MonoBehaviour
     //this is used to transition between diferent AI states
     private Animator animator;
 
+    //A* Seeker component, used for pathfinding
+    private Seeker seeker;
+    public Path path;
+    public float movementSpeed = 1.8f;
+    private int currentWaypoint = 0;
+    public float nextWaypointDistance = 2; //How close we must be to a waypoint before we can go to the next
+    //recalculate path regularly
+    public float repathRate = 0.8f;
+    private float lastRepath = float.NegativeInfinity;
+
     public MeleeEnemyState state;
     public Vector2 direction;
     public float playerDetectionRange = 20;
@@ -22,6 +33,7 @@ public class MeleeEnemyController : MonoBehaviour
     {
         direction = new Vector2(1, 0);
         animator = gameObject.GetComponent<Animator>();
+        seeker = gameObject.GetComponent<Seeker>();
     }
 
     // Use this for initialization
@@ -53,6 +65,7 @@ public class MeleeEnemyController : MonoBehaviour
             case MeleeEnemyState.CHASING:
                 direction = player.transform.position - this.gameObject.transform.position;
                 Rotate();
+                FollowPath();
                 break;
             case MeleeEnemyState.ATTACKING:
                 direction = player.transform.position - this.gameObject.transform.position;
@@ -61,6 +74,97 @@ public class MeleeEnemyController : MonoBehaviour
         }
 
 
+    }
+
+    //Called whenever an A* path for this go is done
+    public void OnPathComplete(Path p)
+    {
+        // Path pooling. To avoid unnecessary allocations paths are reference counted.
+        // Calling Claim will increase the reference count by 1 and Release will reduce
+        // it by one, when it reaches zero the path will be pooled and then it may be used
+        // by other scripts. The ABPath.Construct and Seeker.StartPath methods will
+        // take a path from the pool if possible. See also the documentation page about path pooling.
+        p.Claim(this);
+        if (!p.error)
+        {
+            //if (path != null) path.Release(this);
+            path = p;
+            // Reset the waypoint counter so that we start to move towards the first point in the path
+            currentWaypoint = 0;
+        } else
+        {
+            p.Release(this);
+        }
+    }
+
+    /**
+     * Follows a generated path an creates new paths every x seconds.
+    */
+    public void FollowPath()
+    {
+        //repath from time to time
+        if (Time.time > lastRepath + repathRate && seeker.IsDone())
+        {
+            Debug.Log("REPATHING");
+
+            lastRepath = Time.time;
+
+            // Start a new path to the targetPosition, call the the OnPathComplete function
+            // when the path has been calculated (which may take a few frames depending on the complexity)
+            seeker.StartPath(transform.position, player.transform.position, OnPathComplete);
+        }
+
+
+        if (path == null)
+        {
+            // We have no path to follow yet, so don't do anything
+            return;
+        }
+
+        // Check in a loop if we are close enough to the current waypoint to switch to the next one.
+        // We do this in a loop because many waypoints might be close to each other and we may reach
+        // several of them in the same frame.
+        bool reachedEndOfPath = false;
+        // The distance to the next waypoint in the path
+        float distanceToWaypoint;
+        while (true)
+        {
+            // If you want maximum performance you can check the squared distance instead to get rid of a
+            // square root calculation. But that is outside the scope of this tutorial.
+            distanceToWaypoint = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
+            if (distanceToWaypoint < nextWaypointDistance)
+            {
+                // Check if there is another waypoint or if we have reached the end of the path
+                if (currentWaypoint + 1 < path.vectorPath.Count)
+                {
+                    currentWaypoint++;
+                }
+                else
+                {
+                    // Set a status variable to indicate that the agent has reached the end of the path.
+                    // You can use this to trigger some special code if your game requires that.
+                    reachedEndOfPath = true;
+                    break;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        // Slow down smoothly upon approaching the end of the path
+        // This value will smoothly go from 1 to 0 as the agent approaches the last waypoint in the path.
+        var speedFactor = reachedEndOfPath ? Mathf.Sqrt(distanceToWaypoint / nextWaypointDistance) : 1f;
+
+        // Direction to the next waypoint
+        // Normalize it so that it has a length of 1 world unit
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        // Multiply the direction by our desired speed to get a velocity
+        Vector3 velocity = dir * movementSpeed * speedFactor;
+        
+        // If you are writing a 2D game you may want to remove the CharacterController and instead use e.g transform.Translate
+        transform.position += velocity * Time.deltaTime;
     }
 
     void Rotate()
@@ -84,6 +188,11 @@ public class MeleeEnemyController : MonoBehaviour
     public void StartChasing()
     {
         this.state = MeleeEnemyState.CHASING;
+
+        // Start a new path to the targetPosition, call the the OnPathComplete function
+        // when the path has been calculated (which may take a few frames depending on the complexity)
+        path = null;
+        seeker.StartPath(transform.position, player.transform.position, OnPathComplete);
     }
 
     public void StopChasing()
